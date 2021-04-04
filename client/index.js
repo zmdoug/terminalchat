0fefe26ffdf50d2d82bb4cd18b7d3bb978548eff
Douglas Maehler <douglas.zardinello@ahgora.com.br>
2021-04-04 17:05:47 -0300

    Set connection to the server

--- client/index.js
+++ client/index.js
@@ -8,13 +8,15 @@ node index.js \
 
 import Events from 'events';
 import CliConfig from './src/cliConfig.js';
+import SocketClient from './src/socket.js';
 import TerminalController from "./src/terminalController.js";
 
 const [nodePath, filePath, ...commands] = process.argv;
 const config = CliConfig.parseArguments(commands);
-console.log(config)
+// console.log(config)
 
 const componentEmitter = new Events();
-
+const socketClient = new SocketClient(config);
+await socketClient.initialize();
 // const controller = new TerminalController();
 // await controller.initializeTable(componentEmitter);
