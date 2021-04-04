cb3e9840b27a89988d7fe82683cb1a5735639000
Douglas Maehler <douglas.zardinello@ahgora.com.br>
2021-04-04 16:52:02 -0300

    Setup socket events

--- server/src/index.js
+++ server/src/index.js
@@ -1,38 +1,44 @@
 import SocketServer from './socket.js';
 import Event from 'events';
 import { constants } from './constants.js';
+import Controller from './controller.js';
 
 const eventEmitter = new Event();
 
-// async function testServer() {
-//   const options = {
-//     port: 9898,
-//     host: 'localhost',
-//     headers: {
-//       Connection: 'Upgrade',
-//       Upgrade: 'websocket'
-//     }
-//   }
+async function testServer() {
+  const options = {
+    port: 9898,
+    host: 'localhost',
+    headers: {
+      Connection: 'Upgrade',
+      Upgrade: 'websocket'
+    }
+  }
 
-//   const http = await import('http');
-//   const req = http.request(options);
-//   req.end();
+  const http = await import('http');
+  const req = http.request(options);
+  req.end();
 
-//   req.on('upgrade', (res, socket) => {
-//     socket.on('data', data => {
-//       console.log('client received', data.toString());
-//     });
+  req.on('upgrade', (res, socket) => {
+    socket.on('data', data => {
+      console.log('client received', data.toString());
+    });
 
-//     setInterval(() => {
-//       socket.write('Hello!');
-//     }, 500);
-//   });
-// }
+    setInterval(() => {
+      socket.write('Hello!');
+    }, 500);
+  });
+}
 
 const port = process.env.PORT || 9898;
 const socketServer = new SocketServer({ port });
 const server = await socketServer.initialize(eventEmitter);
 console.log('socket server is running at', server.address().port);
+
+const controller = new Controller({ socketServer });
+
+eventEmitter.on(constants.event.NEW_USER_CONNECTED,
+  controller.onNewConnection.bind(controller)) // bind para recuperar o this do controller
 
 // eventEmitter.on(constants.event.NEW_USER_CONNECTED, (socket) => {
 //   console.log('new connection!!!', socket.id);
@@ -43,4 +49,4 @@ console.log('socket server is running at', server.address().port);
 //   });
 // });
 
-// await testServer();
+await testServer();
