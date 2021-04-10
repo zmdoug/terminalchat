78fe920330eddd66f939efe1c5f72ee4f2b1b55b
Douglas Maehler <douglas.zardinello@ahgora.com.br>
2021-04-09 23:55:47 -0300

    create connection upgrade handShake, connect user to server, keep users event

--- client/index.js
+++ client/index.js
@@ -8,6 +8,7 @@ node index.js \
 
 import Events from 'events';
 import CliConfig from './src/cliConfig.js';
+import EventManager from './src/eventManager.js';
 import SocketClient from './src/socket.js';
 import TerminalController from "./src/terminalController.js";
 
@@ -18,5 +19,15 @@ const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
 const socketClient = new SocketClient(config);
 await socketClient.initialize();
-// const controller = new TerminalController();
-// await controller.initializeTable(componentEmitter);
+const eventManager = new EventManager({ componentEmitter, socketClient });
+const events = eventManager.getEvents();
+socketClient.attachEvents(events);
+
+const data = {
+    roomId: config.room,
+    userName: config.username
+};
+eventManager.joinRoomAndWaitForMessages(data);
+
+const controller = new TerminalController();
+await controller.initializeTable(componentEmitter);
